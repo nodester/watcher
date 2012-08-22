831b97e92a4e0965ca416c86a835cd99e552bba2
Alejandro Morales <vamg008@gmail.com>
2012-08-22 12:42:08 -0600

    [dist] Cleanup, fixed api no more new to start the service

--- examples/child.js
+++ examples/child.js
@@ -1,12 +1,15 @@
-(new (require('node-watcher'))({
+
+require('../')({
   host:'localhost',
   user:'alejandromg',
   password:'34141231',
-  path:'/env/audit'
-})).emitter({
-  maxMemory:25, //25mb,
-  sizeSpace:25,
-  checkSpace:true
+  path:'/env/audit',
+  emitter: {
+    maxMemory: 25, //25mb,
+    sizeSpace: 25,
+    checkSpace: true,
+    interval: 5000
+  }
 })
 
 require('http').createServer(function(req,res){
